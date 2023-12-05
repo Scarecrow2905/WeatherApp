19598d9ed3cbf260ee7c73c3eb4d3469f33992cc
Tommy <tommy-sc@live.no>
2023-12-05 15:46:09 +0100

    Cleaned up boilerplate and added WeatherCard component

---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,28 @@
+<!--Whole file is one component-->
+
+<!--This is where the html goes -->
+<template>
+    <div class="Weather-card">
+        <!--Weather information goes here-->
+        <div>Degrees: {{ Degrees }}°</div>
+        <div>Description: {{ City }}</div>
+    </div>
+</template>
+
+
+<!--This is where the script logic and options goes-->
+<script setup lang="ts">
+import axios from 'axios';
+
+    defineProps<{
+        Degrees?: number,
+        City?: string,
+        
+    }>()
+</script>
+
+
+<!--Component spesific style under here-->
+<style scoped>
+/*Add styles to WeatherCard here */
+</style>
