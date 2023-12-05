46ef9a52167cd1a16b33ddacc9a9a84d2baa6eb9
Tommy <tommy-sc@live.no>
2023-12-05 16:22:27 +0100

    Chore: added fetch api logic in WeatherCard.vue

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -13,12 +13,28 @@
 <!--This is where the script logic and options goes-->
 <script setup lang="ts">
 import axios from 'axios';
+import { onMounted } from 'vue';
 
-    defineProps<{
+   const props = defineProps<{
         Degrees?: number,
         City?: string,
         
-    }>()
+    }>();
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={a1edbad8839196798fba35acf6d06891}')
+            console.log(response.data) // Log for verification
+        }
+        catch (error){
+            console.error("Error fetching weather data", error);   
+        }
+    };
+
+    // Fetch data when the component is mounted
+    onMounted(fetchData);
+
+
 </script>
 
 
