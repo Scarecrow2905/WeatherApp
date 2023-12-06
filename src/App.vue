49c6635f5335d7808c585ed29560aef058c64935
Tommy <tommy-sc@live.no>
2023-12-06 13:00:20 +0100

    Chore: Refactoring code in the API request, still needs some work

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,26 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <div id="app">
-      <WeatherCard msg="Weather app works!" />
-    </div>
-  </header> 
+	<header>
+		<div id="app">
+			<WeatherCard msg="Weather app works!" />
+		</div>
+	</header>
 </template>
 
 <script>
-import WeatherCard from './components/WeatherCard.vue';
+import WeatherCard from "./components/WeatherCard.vue";
 
 export default {
-  components: {
-    WeatherCard
-  },
+	components: {
+		WeatherCard,
+	},
 };
-
 </script>
 
 <style scoped>
 header {
-  /*Header styling goes here */ 
-  line-height: 1.5;
+	/*Header styling goes here */
+	line-height: 1.5;
 }
-
-
 </style>
--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -2,43 +2,66 @@
 
 <!--This is where the html goes -->
 <template>
-    <div class="Weather-card">
-        <!--Weather information goes here-->
-        <div>Degrees: {{ Degrees }}°</div>
-        <div>Description: {{ City }}</div>
-    </div>
-</template>
+	<div class="searh-box">
+		<input
+			type="text"
+			class="search-bar"
+			placeholder="Search.."
+			v-model="query"
+			@input="fetchData"
+		/>
+		{{ query }}
+	</div>
 
+	<!--Weather information goes here-->
+
+	<div class="weather-card">
+		<div class="location-box">
+			<div class="location">City: {{ city }}</div>
+			<div class="date">Date: {{ date }}</div>
+		</div>
+
+		<div class="weather-box">
+			<div class="temp">Temp: {{ temp }}</div>
+			<div class="weather">Weather: {{ weather }}</div>
+		</div>
+	</div>
+</template>
 
 <!--This is where the script logic and options goes-->
 <script setup lang="ts">
-import axios from 'axios';
-import { onMounted } from 'vue';
+import axios from "axios";
+import { ref, onMounted } from "vue";
 
-   const props = defineProps<{
-        Degrees?: number,
-        City?: string,
-        
-    }>();
+const query = ref("");
+const city = ref("");
+const date = ref("");
+const temp = ref("");
+const weather = ref("");
 
-    const fetchData = async () => {
-        try {
+const apiUrl = "https://api.openweathermap.org/data/2.5/";
+const apiKey = "a1edbad8839196798fba35acf6d06891";
 
-            // Check youtube for how the api request looked like!!
-            const response = await axios.get('')
-            console.log(response.data) // Log for verification
-        }
-        catch (error){
-            console.error("Error fetching weather data", error);   
-        }
-    };
+const fetchData = async () => {
+	try {
+		// Check youtube for how the api request looked like
+		const response = await axios.get(apiUrl, {
+			params: {
+				q: query.value,
+				appid: apiKey,
+			},
+		});
+		console.log(response.data); // Log for verification
+	} catch (error) {
+		console.error("Error fetching weather data", error);
+	}
+};
 
-    // Fetch data when the component is mounted
-    onMounted(fetchData);
+// Fetch data when the component is mounted
+onMounted(fetchData);
 
-
+export { query, city, date, temp, weather };
 </script>
-
 
 <!--Component spesific style under here-->
 <style scoped>
