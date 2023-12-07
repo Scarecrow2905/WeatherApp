b08b262c6818e734797b12d877f849e1f5650d5c
Tommy <tommy-sc@live.no>
2023-12-07 18:08:14 +0100

    Chore: More troubleshooting, no success. But did some refactoring, will continue tomorrow

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -8,59 +8,60 @@
 			class="search-bar"
 			placeholder="Search.."
 			v-model="query"
-			@input="fetchData"
+			@keypress.Enter="fetchData"
 		/>
-		{{ query }}
 	</div>
+	{{ query }}
 
 	<!--Weather information goes here-->
 
-	<div class="weather-card">
+	<div class="weather-card" v-if="typeof query != 'undefined'">
 		<div class="location-box">
-			<div class="location">City: {{ city }}</div>
-			<div class="date">Date: {{ date }}</div>
+			<div class="location">City: {{ weather.name }}</div>
+			<div class="date">Date: {{ weather.date }}</div>
 		</div>
 
 		<div class="weather-box">
-			<div class="temp">Temp: {{ temp }}</div>
-			<div class="weather">Weather: {{ weather }}</div>
+			<div class="temp">Temp: {{ weather.temp }}</div>
+			<div class="weather">Weather: {{}}</div>
 		</div>
 	</div>
 </template>
 
 <!--This is where the script logic and options goes-->
-<script setup lang="ts">
+<script lang="ts">
 import axios from "axios";
 import { ref, onMounted } from "vue";
 
-const query = ref("");
-const city = ref("");
-const date = ref("");
-const temp = ref("");
-const weather = ref("");
+export default {
+	setup() {
+		const query = ref("");
+		const weather = ref<any>(null);
 
-const apiUrl = "https://api.openweathermap.org/data/2.5/";
-const apiKey = "a1edbad8839196798fba35acf6d06891";
+		const apiUrl = "https://api.openweathermap.org/data/2.5/";
+		const apiKey = "a1edbad8839196798fba35acf6d06891";
 
-const fetchData = async () => {
-	try {
-		// Check youtube for how the api request looked like
-		const response = await axios.get(apiUrl, {
-			params: {
-				q: query.value,
-				appid: apiKey,
-			},
-		});
-		console.log(response.data); // Log for verification
-	} catch (error) {
-		console.error("Error fetching weather data", error);
-	}
+		const fetchData = async () => {
+			try {
+				// Check youtube for how the api request looked like
+				const response = await axios.get(apiUrl + "weather", {
+					params: {
+						q: query.value,
+						appid: apiKey,
+					},
+				});
+				console.log(response.data); // Log for verification
+			} catch (error) {
+				console.error("Error fetching weather data", error);
+			}
+		};
+
+		// Fetch data when the component is mounted
+		onMounted(fetchData);
+
+		return { fetchData, apiKey, apiUrl, query, weather };
+	},
 };
-
-// Fetch data when the component is mounted
-onMounted(fetchData);
-
-export { query, city, date, temp, weather };
 </script>
 
 <!--Component spesific style under here-->
